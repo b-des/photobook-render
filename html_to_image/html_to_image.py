6385dba8298819b64fe0d70b0d63f7468d7bdd8c
b-des <andrii.protsenko@masterofcode.com>
2020-02-22 09:51:33 +0200

    Update file path in response

--- html_to_image/html_to_image.py
+++ html_to_image/html_to_image.py
@@ -8,15 +8,15 @@ render_url = 'https://{}/index.php?route=photobook/photobook/renderPage&uid={}&p
 
 options = {
     'window-status': 'ready',
-    #'quiet': '',
+    # 'quiet': '',
     'quality': 100,
     'format': 'jpg'
 }
 
 
-def destination_file(domain, uid, filename=None):
+def destination_file(domain, uid, filename=None, include_os_path=True):
     try:
-        path = os.path.join(domains[domain], 'photobook/snapshots', uid)
+        path = os.path.join(domains[domain] if include_os_path is True else domain, 'photobook/snapshots', uid)
     except KeyError:
         return None
 
@@ -41,8 +41,8 @@ def slice_page(page, pages, domain, uid):
         sliced[0].image.save(destination_file(domain, uid, '{}-original'.format(pages * 2)))
         sliced[1].image.save(destination_file(domain, uid, '1-original'))
 
-        #sliced[0].image.crop((10, 10, 500, 500 - 10)).save(destination_file(domain, uid, pages * 2), quality=100)
-        #sliced[1].image.crop((10, 10, 500, 500 - 10)).save(destination_file(domain, uid, 1), quality=100)
+        # sliced[0].image.crop((10, 10, 500, 500 - 10)).save(destination_file(domain, uid, pages * 2), quality=100)
+        # sliced[1].image.crop((10, 10, 500, 500 - 10)).save(destination_file(domain, uid, 1), quality=100)
         sliced[0].image.crop((10, 10, 500, 490)).save(destination_file(domain, uid, pages * 2), quality=100)
         sliced[1].image.crop((0, 10, 490, 490)).save(destination_file(domain, uid, 1), quality=100)
     else:
@@ -52,7 +52,7 @@ def slice_page(page, pages, domain, uid):
         else:
             sliced[0].image.save(destination_file(domain, uid, number), quality=100)
 
-        if number + 1 != pages*2-1:
+        if number + 1 != pages * 2 - 1:
             sliced[1].image.crop((0, 10, 490, 490)).save(destination_file(domain, uid, number + 1), quality=100)
         else:
             sliced[1].image.save(destination_file(domain, uid, number + 1), quality=100)
@@ -115,10 +115,10 @@ def create_borders(pages, domain, uid):
     os.remove(destination_file(domain, uid, '{}-original'.format(pages * 2)))
 
 
-def create_response(destination, domain):
+def create_response(destination, url_path):
     f = []
     for (dirpath, dirnames, filenames) in os.walk(destination):
-        [f.extend([os.path.join(domain, file)]) for file in filenames]
+        [f.extend([os.path.join(url_path, file)]) for file in filenames]
         break
     return {'data': f, 'code': 200}
 
@@ -158,6 +158,12 @@ def make_previews(pages=0, uid='', domain='', size=None, is_user_preview=False):
     # don't create borders
     if is_user_preview is False:
         create_borders(pages, domain, uid)
-        return create_response(destination_file(domain, uid), domain)
+        return create_response(
+            destination_file(domain, uid),
+            destination_file(domain, '%s/%s' % (uid, 'preview'), False)
+        )
     else:
-        return create_response(destination_file(domain, '%s/%s' % (uid, 'preview')), domain)
+        return create_response(
+            destination_file(domain, '%s/%s' % (uid, 'preview')),
+            destination_file(domain, '%s/%s' % (uid, 'preview'), False)
+        )
