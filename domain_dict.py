c811be4b78ce15c44b7bb83d571e1bff67c32a0c
b-des <andrii.protsenko@masterofcode.com>
2020-02-26 20:59:28 +0200

    Add site's paths to docker-compose file

--- domain_dict.py
+++ domain_dict.py
@@ -1,4 +1,14 @@
 domains = {
     'pechat.photo': '/var/www/demonstration/data/www/pechat.photo/',
-    #'pechat.photo': 'images',
+    'casy.com.ua': '/var/www/casycomua/data/www/casy.com.ua',
+    'digitalfoto.pp.ua': '/var/www/digitalfotoua/data/www/digitalfoto.pp.ua',
+    'evolvestudio.md': '/var/www/fotocarte/data/www/evolvestudio.md',
+    'foto124.ru': '/var/www/foto124/data/www/foto124.ru',
+    'knigoboomplus.ru': '/var/www/knigoboomplus/data/www/knigoboomplus.ru',
+    'memory-moments.online': '/var/www/borovodessa/data/www/memory-moments.online',
+    'online.photolier.ru': '/var/www/photolier/data/www/online.photolier.ru',
+    'pechatfotoonline.ru': '/var/www/pechatfotoonline/data/www/pechatfotoonline.ru',
+    'photo-mich.ru': '/var/www/photomich/data/www/photo-mich.ru',
+    'photopechat23.ru': '/var/www/urakrasnodar/data/www/photopechat23.ru',
+    'xn--80aj0aafqej6a4c.xn--80asehdb': '/var/www/admin/data/www/xn--80aj0aafqej6a4c.xn--80asehdb',
 }
